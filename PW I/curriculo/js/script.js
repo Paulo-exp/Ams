a51a7cdf10608fa10a840ea2ea5542368e247c89
Paulo-exp <paulinho899gamer2@gmail.com>
2026-05-07 13:44:13 -0300

    Add files via upload

--- PW I/curriculo/js/script.js
+++ PW I/curriculo/js/script.js
@@ -7,8 +7,9 @@ if (localStorage.getItem("Tema") === "dark" ){
 botao.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     if (document.body.classList.contains("dark")) {
-        localStorage.setItem("tema", "dark");
+        localStorage.setItem("Tema", "dark");
     } else {
-        localStorage.setItem("tema", "light");
+        localStorage.setItem("Tema", "light");
     }
 });
+
